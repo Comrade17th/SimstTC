8a38a9f5eef49b6f6949e91e0f91ef6be2fdb4c6
nasusfen <ravshannedonat@gmail.com>
2024-06-11 23:24:21 +0300

    Floor-1 labeled

--- Assets/_Project/Sources/Windows/SquareInfoWindow.cs
+++ Assets/_Project/Sources/Windows/SquareInfoWindow.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 public class SquareInfoWindow : Window
 {
-    
+    [SerializeField] private Text _organizationName;
+    [SerializeField] private Text _square;
+    [SerializeField] private Text _price;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(_organizationName);
+        Assert.IsNotNull(_square);
+        Assert.IsNotNull(_price);
+    }
+
+    public void ChangeInfo(string text)
+    {
+        string[] elements = text.Split('|');
+        
+        if (elements.Length == 3)
+        {
+            _organizationName.text = elements[0];
+            _square.text = elements[1];
+            _price.text = elements[2];
+
+        }
+        else
+        {
+            Debug.Log($"SquareInfoWindow, low count of input");
+        }
+    }
 }
