46a2010058ff1b5eed2c481933b7ed705c8c529a
nasusfen <ravshannedonat@gmail.com>
2024-06-11 19:08:23 +0300

    Charts loaded, payment done

--- Assets/_Project/Sources/Windows/PaymentWindow.cs
+++ Assets/_Project/Sources/Windows/PaymentWindow.cs
@@ -7,7 +7,7 @@ public class PaymentWindow : Window
 {
     private void OpenPaymentUrl()
     {
-        Application.OpenURL($"https://online.sberbank.ru/CSAFront/index.do");
+        Application.OpenURL($"https://online.vtb.ru/login?utm_source=yandex&utm_medium=cpc&utm_campaign=mgcom-mobile_yd_vtbo_web_brand_search%7C107888083&utm_content=cid%7C107888083%7Cgid%7C5409301742%7Caid%7C15884501393%7C50735662038_50735662038%7Ccrt_0%7Cdvc%7Cdesktop%7Creg%7CКазань&utm_term=---autotargeting&yclid=1967274824980234239");
     }
 
     protected override void OnNextWindowButtonClick()
